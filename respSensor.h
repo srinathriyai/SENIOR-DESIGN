17944c0f6c764d40d36b4724f543569cdf4811f8
apapi001 <apapi001@ucr.edu>
2025-10-21 11:36:10 -0700

    Update respSensor.h

---
+++ respSensor.h
@@ -0,0 +1,6 @@
+//Respiratory Sensor block
+//takes analog voltage for ___ seconds and generates ____ samples
+//might need some data parsing
+//stores average data as ____ variable (name)
+
+#include <Arduino.h>
