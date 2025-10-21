7b61a40bb9f34400cca9fce54278609c5777c278
apapi001 <apapi001@ucr.edu>
2025-10-21 11:36:59 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,9 @@
 #include <Arduino.h>
+#include <tempSensor.h>
+#include <hrSensor.h>
+#include <respSensor.h>
 
-// put function declarations here:
-int myFunction(int, int);
+//global variables
+//variables or other needed tied to sensor header files
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
