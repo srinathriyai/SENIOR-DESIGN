2264c00cd60b00bd823eba90d6547fc67016e4a6
apapi001 <apapi001@ucr.edu>
2025-10-21 11:36:43 -0700

    Update hrSensor.h

---
+++ hrSensor.h
@@ -0,0 +1,9 @@
+//Heart Rate and Oxygen Sensor block
+//takes heart rate/pulse for ___ seconds and generates ____ samples
+//stores average heart rate data as ____ variable (name)
+
+//takes red and infrared LED data for ___ seconds and generates ____ samples
+//stores average data for both LED as ____ variable and ____ variable (names)
+//calculates spO2 based on ratio
+
+#include <Arduino.h>
