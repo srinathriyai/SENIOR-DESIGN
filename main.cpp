f412e93fcbdfdfe3bdcbba4e19204da11904e42e
apapi001 <apapi001@ucr.edu>
2025-10-21 11:52:05 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,41 @@
 #include <tempSensor.h>
 #include <hrSensor.h>
 #include <respSensor.h>
+//other includes? interrupt etc as needed
+
 
 //global variables
-//variables or other needed tied to sensor header files
+  //timer variable
+  //
 
 
+//functions
+  //confirm sensor function (yellow on functional block diagram)
+  //confirm server connection (^^^)
+
+  //take sample function (call to headers) and send to server 
+
+  //function for combining LLM risk assessment
+
+  //function(s) for summary wording from LLM risk assessment
+
+
+
+int main(void){
+//variables
+
+//check BT/wifi module connection 
+
+//confirm sensor function
+//confirm server function
+
+//timer loop 
+  //take sample function + send data
+
+  //receive back data values for ranking (for input to wording summary)
+    //function for wording summary (takes ranking values as input)
+    
+
+
+
+}
