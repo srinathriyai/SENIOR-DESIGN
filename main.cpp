07e7790a7330223280e9ef25e441f387a2a9f693
apapi001 <apapi001@ucr.edu>
2025-10-25 21:23:42 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,6 @@
 #include <respSensor.h>
 //other includes? interrupt etc as needed
 
-int testHrVal = 0;
-int testtempVal = 0;
-int testRespVal = 0;
-int testBpVal = 0;
-//set values within individual respective functions to mimic getting from sensors
-//confirm good/bad values respond correctly (no server/risk assessment right now, connect later)
 
 //global variables
   //timer variable
@@ -26,7 +20,44 @@ int testBpVal = 0;
 
   //function(s) for summary wording from LLM risk assessment
 
+unsigned int sampleHeartRate(int avgHr){    //Converting heart rate average to rank severity score and outputting
+  int hrRank;                               //For storing rank
 
+  if(avgHr >= 1){       //1 for testing, signifying good
+    hrRank = 0;         //rank 0 meaning safe range
+  }
+  else if(avgHr == 0){  //0 for testing, signifying bad
+    hrRank = 1;         //rank 1 meaning bad
+  }
+
+  return hrRank;  //output
+}
+
+unsigned int sampleRespiratory(int avgResp){  //Converting respiratory average to rank severity score and outputting
+  int respRank;                               //For storing rank
+
+  if(avgResp >= 1){     //1 for testing, signifying good
+    respRank = 0;       //rank 0 meaning safe range
+  }
+  else if(avgResp == 0){
+    respRank = 1;
+  }
+
+  return respRank;  //output
+}
+
+unsigned int sampleTemperature(int avgTemp){  //Converting temperature average to rank severity score and outputting
+  int tempRank;
+
+  if(avgTemp >= 1){     //1 for testing, signifying good
+    tempRank = 0;       //rank 0 meaning safe range
+  }
+  else if(avgTemp == 0){
+    avgTemp = 1;          //rank 1 meaning bad
+  }
+
+  return tempRank;
+}
 
 int main(void){
 //variables
@@ -43,7 +74,18 @@ int main(void){
     //function for wording summary (takes ranking values as input)
     
 
+//testing code for output, assuming values are taken as averages from individual sensor block codes
+
+  //dummy variables for results from sampling ( replace with calling to sensor header files )
+  int sampledHrAvg = 1;   
+  int sampledRespAvg = 0; 
+  int sampledTempAvg = 1;
+
+  //take samples
+  sampleHeartRate(sampledHrAvg); 
+  sampleRespiratory(sampledRespAvg);
+  sampleTemperature(sampledTempAvg);
 
 
+  return 0;
 }
-
