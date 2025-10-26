2284c67b8baae902c5a69587df655ca74dc30bf3
apapi001 <apapi001@ucr.edu>
2025-10-25 23:05:02 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
-#include <Arduino.h>
-#include <tempSensor.h>
-#include <hrSensor.h>
-#include <respSensor.h>
+//#include <Arduino.h>
+#include <stdio.h> //here for testing, CHANGED platform.ini to framework = espidf  instead of arduino
+#include <string.h>
+//IF ARDUINO PLUGGED IN CHANGE ^^^^^
+
+//#include <tempSensor.h>
+//#include <hrSensor.h>
+//#include <respSensor.h>
+//#include <bp.h>
 //other includes? interrupt etc as needed
 
 
-//global variables
+//global variables and definitions
   //timer variable
-  //
-
+  //define ranking variables?, for sample rank output of functions instead of defining in functions
 
 //functions
   //confirm sensor function (yellow on functional block diagram)
@@ -53,13 +57,59 @@ unsigned int sampleTemperature(int avgTemp){  //Converting temperature average t
     tempRank = 0;       //rank 0 meaning safe range
   }
   else if(avgTemp == 0){
-    avgTemp = 1;          //rank 1 meaning bad
+    tempRank = 1;          //rank 1 meaning bad
   }
 
   return tempRank;
 }
 
-int main(void){
+void summaryResults(int hr, int resp, int temp){   //Take sample ranks and print summary of results (make more wording later on)
+//Testing block just for now
+  char sumHr[10];
+  char sumResp[10];
+  char sumTemp[10];
+//if else statements for safe and unsafe
+  if(hr >= 1){
+    strcpy(sumHr, "safe");    //dummy string
+  }
+  else if(hr == 0){
+    strcpy(sumHr, "unsafe");
+  }
+
+  if(resp >= 1){
+    strcpy(sumResp, "safe");
+  }
+  else if(resp == 0){
+    strcpy(sumResp, "unsafe");
+  }
+
+  if(temp >= 1){
+    strcpy(sumTemp, "safe");
+  }
+  else if(temp == 0){
+    strcpy(sumTemp, "unsafe");
+  }
+
+  //output print, can format better later
+  printf("Patient's Heart Rate is within %s range.\n", sumHr); printf("Patient's Respiratory Rate is within %s range.\n", sumResp); printf("Patient's Temperature is within %s range.\n", sumTemp);
+  //this is arduino format below
+  /*
+  Serial.print("Patient's Heart Rate is within ");
+  Serial.print(sumHr);
+  Serial.println(" range.");
+
+  Serial.print("Patient's Respiratory Rate is within ");
+  Serial.print(sumResp);
+  Serial.println(" range.");
+
+  Serial.print("Patient's Temperature is within ");
+  Serial.print(sumTemp);
+  Serial.println(" range.");
+  */
+}
+
+int main(){            //need to be changed to void setup(){} for arduino
+//Serial.begin(115200);   //esp32, uncomment if plugged to arduino
 //variables
 
 //check BT/wifi module connection 
@@ -82,10 +132,17 @@ int main(void){
   int sampledTempAvg = 1;
 
   //take samples
-  sampleHeartRate(sampledHrAvg); 
-  sampleRespiratory(sampledRespAvg);
-  sampleTemperature(sampledTempAvg);
+  int hrRank = sampleHeartRate(sampledHrAvg); 
+  int respRank = sampleRespiratory(sampledRespAvg);
+  int tempRank = sampleTemperature(sampledTempAvg);
 
+  //summary results
+  summaryResults(hrRank, respRank, tempRank);
 
   return 0;
+}
+
+
+void loop() { //requried for arduino include
+  //can be left empty for now
 }
