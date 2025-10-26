3904ab46507bd9491ee9e69b3268c744567931f7
Samuel42888 <sli428@ucr.edu>
2025-10-26 10:45:48 -0700

    Update bp.h

--- bp.h
+++ bp.h
@@ -3,3 +3,16 @@
 //stores average blood pressure data as ____ variable (name)
 
 #include <Arduino.h>
+
+// FOR CIRCUIT DIAGRAM REFER TO: FEASIBILITY STUDY - BP MONITOR CIRCUIT DIAGRAM
+// Initialize 2, 3, 4, 5, SCL, and SDA pins
+
+// Activate pump
+// Set pin 3 (In2) to high
+
+// Activate solenoid
+// Set pin 5 (In1) to high
+
+// Take readings from BP sensor
+// Read from pins SCL and SDA using I2C
+
