bf44c0265bf879e49c734519d18debe5cdcea0d1
Samuel42888 <sli428@ucr.edu>
2025-10-27 19:18:46 -0700

    Update bp.h

--- bp.h
+++ bp.h
@@ -17,3 +17,20 @@
 // Take readings from BP sensor
 // Read from pins SCL and SDA using I2C
 
+#define RESET_PIN -1
+#define EOC_PIN
+Adafruit_MPRLS mpr = Adafruit_MPRLS(RESET_PIN, EOC_PIN);
+
+int main() {
+  // Doing in main without states for now to test basic interaction
+  serial.begin
+  if (!mpr.begin()) { // Calls mpr.begin() (see Adafruit_MPRLS::begin) and will output a 0 if it could not begin the sensor
+    serial.print("Pressure sensor check failed")
+  } else {
+    serial.print("Pressure sensor check passed")
+  }
+  float pressure = mpr.readpressure();
+  serial.println(pressure); // in hectopascals (hPa) // 133.32 hPa = 1 mmHg
+
+  return 0;
+}
