aa8f9ae04ddec34245a9808b29eb2af68ac29912
apapi001 <apapi001@ucr.edu>
2025-10-30 12:10:49 -0700

    Update tempSensor.h

--- tempSensor.h
+++ tempSensor.h
@@ -2,4 +2,8 @@
 //takes temperature for ___ seconds and generates ____ samples
 //stores average data as ____ variable (name)
 
-#include <Arduino.h> 
+//#include <Arduino.h>
+#include <stdio.h> //here for testing, CHANGED platform.ini to framework = espidf  instead of arduino
+#include <string.h>
+//IF ARDUINO PLUGGED IN CHANGE ^^^^^
+
