cd54eef7ecd5648afe9479823240fe58669755b0
apapi001 <apapi001@ucr.edu>
2025-11-01 20:18:13 -0700

    Update tempSensor.h

--- tempSensor.h
+++ tempSensor.h
@@ -8,68 +8,49 @@
 //IF ARDUINO PLUGGED IN CHANGE ^^^^^
 
 //FROM Arduino library 
-/*!
- * @file        getData.ino
- * @brief       this demo demonstrates how to put the sensor enter/exit sleep mode and get temperature data measured by sensor
- * @copyright   Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
- * @license     The MIT License (MIT)
- * @author      [qsjhyy](yihuan.huang@dfrobot.com)
- * @version     V1.0
- * @date        2021-08-09
- * @url         https://github.com/DFRobot/DFRobot_MLX90614
- */
-#include <DFRobot_MLX90614.h>
+/***************************************************
+  This is a library example for the MLX90614 Temp Sensor
 
-DFRobot_MLX90614_I2C sensor;   // instantiate an object to drive our sensor
+  Designed specifically to work with the MLX90614 sensors in the
+  adafruit shop
+  ----> https://www.adafruit.com/products/1747 3V version
+  ----> https://www.adafruit.com/products/1748 5V version
 
-void setup()
-{
-  Serial.begin(115200);
+  These sensors use I2C to communicate, 2 pins are required to
+  interface
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
 
-  // initialize the sensor
-  while( NO_ERR != sensor.begin() ){
-    Serial.println("Communication with device failed, please check connection");
-    delay(3000);
-  }
-  Serial.println("Begin ok!");
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
 
-  /**
-   * adjust sensor sleep mode
-   * mode select to enter or exit sleep mode, it's enter sleep mode by default
-   *      true is to enter sleep mode
-   *      false is to exit sleep mode (automatically exit sleep mode after power down and restart)
-   */
-  sensor.enterSleepMode();
-  delay(50);
-  sensor.enterSleepMode(false);
-  delay(200);
+#include <Adafruit_MLX90614.h>
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("Adafruit MLX90614 test");
+
+  if (!mlx.begin()) {
+    Serial.println("Error connecting to MLX sensor. Check wiring.");
+    while (1);
+  };
+
+  Serial.print("Emissivity = "); Serial.println(mlx.readEmissivity());
+  Serial.println("================================================");
 }
 
-void loop()
-{
-  /**
-   * get ambient temperature, unit is Celsius
-   * return value range： -40.01 °C ~ 85 °C
-   */
-  float ambientTemp = sensor.getAmbientTempCelsius();
-
-  /**
-   * get temperature of object 1, unit is Celsius
-   * return value range： 
-   * @n  -70.01 °C ~ 270 °C(MLX90614ESF-DCI)
-   * @n  -70.01 °C ~ 380 °C(MLX90614ESF-DCC)
-   */
-  float objectTemp = sensor.getObjectTempCelsius();
-
-  // print measured data in Celsius
-  Serial.print("Ambient celsius : "); Serial.print(ambientTemp); Serial.println(" °C");
-  Serial.print("Object celsius : ");  Serial.print(objectTemp);  Serial.println(" °C");
-
-  // print measured data in Fahrenheit
-  Serial.print("Ambient fahrenheit : "); Serial.print(ambientTemp*9/5 + 32); Serial.println(" F");
-  Serial.print("Object fahrenheit : ");  Serial.print(objectTemp*9/5 + 32);  Serial.println(" F");
+void loop() {
+  Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempC());
+  Serial.print("*C\tObject = "); Serial.print(mlx.readObjectTempC()); Serial.println("*C");
+  Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempF());
+  Serial.print("*F\tObject = "); Serial.print(mlx.readObjectTempF()); Serial.println("*F");
 
   Serial.println();
   delay(500);
 }
-
