d687d0e3ed93d856df21d1eb606f6b3da58d03ed
apapi001 <apapi001@ucr.edu>
2025-11-01 20:08:11 -0700

    Update hrSensor.h

--- hrSensor.h
+++ hrSensor.h
@@ -1,12 +1,11 @@
-//Heart Rate and Oxygen Sensor block
+//Heart Rate Sensor block
 //takes heart rate/pulse for ___ seconds and generates ____ samples
 //stores average heart rate data as ____ variable (name)
 
-//takes red and infrared LED data for ___ seconds and generates ____ samples
-//stores average data for both LED as ____ variable and ____ variable (names)
-//calculates spO2 based on ratio
+
 
 //#include <Arduino.h>
 #include <stdio.h> //here for testing, CHANGED platform.ini to framework = espidf  instead of arduino
 #include <string.h>
 //IF ARDUINO PLUGGED IN CHANGE ^^^^^
+
