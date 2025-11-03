2ed7440b04f73f5e577698a1a07d48f97b414e5c
Samuel42888 <sli428@ucr.edu>
2025-11-03 15:51:23 -0800

    Update bp.h

--- bp.h
+++ bp.h
@@ -4,8 +4,12 @@
 
 #include "Adafruit_MPRLS.h"
 #include "timerISR.h"
-#include <stdlib.h>
 #include <Arduino.h>
+
+// To be implemented later
+void TimerISR() {
+  TimerFlag = 1;
+}
 
 // FOR CIRCUIT DIAGRAM REFER TO: FEASIBILITY STUDY - BP MONITOR CIRCUIT DIAGRAM, DONE ON ARDUINO UNO
 // Initialize 2, 3, 4, 5, SCL, and SDA pins
@@ -20,12 +24,12 @@
 // Read from pins SCL and SDA using I2C
 
 #define RESET_PIN -1
-#define EOC_PIN
+#define EOC_PIN -1
 Adafruit_MPRLS mpr = Adafruit_MPRLS(RESET_PIN, EOC_PIN);
 
  // Doing in main without states for now to test basic interaction
 int main() {
-  int value = 0; // For reading inputs
+  //int value = 0; // For reading inputs
   
   // Valve and Pump
   // Initialization (ARDUINO UNO)
@@ -36,22 +40,22 @@ int main() {
   delay(2000);
   // Valve
   digitalWrite(in1, HIGH); digitalWrite(in2, LOW); // VALVE ON
-  serial.println("Valve Opened");
+  Serial.println("Valve Opened");
   // Pump
   digitalWrite(in3, HIGH); digitalWrite(in4, LOW); // PUMP ON
   delay(2000);
-  serial.println("Motor Started");
+  Serial.println("Motor Started");
   digitalWrite(in3, LOW); digitalWrite(in4, LOW); // PUMP OFF
 
   // Pressure Sensor
-  serial.begin
+  Serial.begin(115200);
   if (!mpr.begin()) { // Calls mpr.begin() (see Adafruit_MPRLS::begin) and will output a 0 if it could not begin the sensor
-    serial.print("Pressure sensor check failed");
+    Serial.print("Pressure sensor check failed");
   } else {
-    serial.print("Pressure sensor check passed");
+    Serial.print("Pressure sensor check passed");
   }
-  float pressure = mpr.readpressure();
-  serial.println(pressure); // in hectopascals (hPa) // 133.32 hPa = 1 mmHg
+  float pressure = mpr.readPressure();
+  Serial.println(pressure); // in hectopascals (hPa) // 133.32 hPa = 1 mmHg
 
   return 0;
 }
