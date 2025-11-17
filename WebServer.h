79611ef6004724dc7696cff5324f8843e1e73d10
srinathriyai <95508759+srinathriyai@users.noreply.github.com>
2025-11-16 18:52:52 -0800

    Fix header guard in WebServer.h

--- WebServer.h
+++ WebServer.h
@@ -5,6 +5,7 @@
 #include "httplib.h"
 #include "Risk_Assessment.h"
 
+// Launches the web dashboard at http://localhost:5000
 void startWebServer(const Vitals& current, const std::string& llmResponse);
 
 #endif
