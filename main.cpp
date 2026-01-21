e33d709c02b0d6b8779c479f7a9f284e572829bb
Samuel42888 <sli428@ucr.edu>
2026-01-21 12:22:27 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include "HR_Sensor.h"
 #include "TEMP_Sensor.h"
+#include "BP.h"
 
 //tracking simultaneous trigger
 bool lastD2State = HIGH;
@@ -16,10 +17,67 @@ void setup() {
     Serial.println("\n=== READY ===");
     Serial.println("Press D2 to start BOTH measurements simultaneously");
     Serial.println("OR press D4 for HR only");
+    
+    //=============================================================================
+    // BP SETUP
+    //=============================================================================
+    // Initialization (ESP 32)
+    pinMode(in1, OUTPUT); digitalWrite(in1, LOW); // RELEASE ON
+    pinMode(in2, OUTPUT); digitalWrite(in2, LOW); // ^
+    pinMode(in3, OUTPUT); digitalWrite(in3, LOW); // MOTOR OFF
+    pinMode(in4, OUTPUT); digitalWrite(in4, LOW); // ^
+
+    unsigned char i = 0;
+    tasks[i].elapsedTime = sample_pressure_PERIOD;
+    tasks[i].period = sample_pressure_PERIOD;
+    tasks[i].state = sample_pressure_INIT;
+    tasks[i].TickFct = &tick_sample_pressure;
+    ++i;
+    tasks[i].elapsedTime = release_valve_PERIOD;
+    tasks[i].period = release_valve_PERIOD;
+    tasks[i].state = release_valve_INIT;
+    tasks[i].TickFct = &tick_release_valve;
+    ++i;
+    tasks[i].elapsedTime = air_pump_PERIOD;
+    tasks[i].period = air_pump_PERIOD;
+    tasks[i].state = air_pump_INIT;
+    tasks[i].TickFct = &tick_air_pump;
+    ++i;
+    tasks[i].elapsedTime = start_button_PERIOD;
+    tasks[i].period = start_button_PERIOD;
+    tasks[i].state = start_button_INIT;
+    tasks[i].TickFct = &tick_start_button;
+
+    Serial.flush();
+    
+    if (!mpr.begin()) { // Calls mpr.begin() (see Adafruit_MPRLS::begin) and will output a 0 if it could not begin the sensor
+        Serial.print("Pressure sensor check failed\n");
+        Serial.flush();
+        while(1) {
+        Serial.print("Stopped, Restart\n");
+        Serial.flush();
+        delay(1000);
+        }
+    } else {
+        Serial.print("Pressure sensor check passed\n");
+        Serial.flush();
+    }
+    //=============================================================================
 }
 
 void loop() {
-
+    //=============================================================================
+    // BP TASKS
+    //=============================================================================
+    currentmillis = millis();
+    for (unsigned int i = 0; i < NUM_TASKS; i++) {
+        if (currentmillis - tasks[i].elapsedTime >= tasks[i].period) {
+        tasks[i].state = tasks[i].TickFct(tasks[i].state); // Runs the tick and then sets the output state to the new state.
+        tasks[i].elapsedTime = currentmillis;
+        }
+    }
+    //=============================================================================
+    
     bool currentD2 = digitalRead(2);    //checking button on D2 for press while another button is pressed
     if(lastD2State == HIGH && currentD2 == LOW){
         delay(50);
