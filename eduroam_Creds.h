b072d94b9742a2c60866e24bb9ce877c63927102
srinathriyai <hsrin006@ucr.edu>
2026-03-09 11:17:06 -0700

    remove from test/sim

--- eduroam_Creds.h
+++
@@ -1,4 +0,0 @@
-const char* EAP_PASSWORD = "Password"; // Password
-const char* EAP_USERNAME = "sli428"; // NetID
-const char* EAP_IDENTITY = "sli428@ucr.edu"; // UCR email
-const char* WIFI_SSID = "eduroam";
